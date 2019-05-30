475cc5539a7ee17211904a9ed6ed9795195c15ae
Wes Weirather <weirdev@outlook.com>
2019-05-30 12:06:16 -0500

    Fixed issues with matrix stochastic simulation.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::io;
+
+#[macro_use(array)]
+extern crate ndarray;
+
 use ndarray::{Array, Array2, Axis};
 use rand::prelude::*;
+use rand::distributions::{Poisson, Gamma};
 
 #[derive(Clone)]
 #[derive(Copy)]
+#[derive(Debug)]
 enum AgentStatus {
     Asymptomatic,
     //Symptomatic,
@@ -12,12 +19,14 @@ enum AgentStatus {
 
 #[derive(Clone)]
 #[derive(Copy)]
+#[derive(Debug)]
 enum InfectionStatus {
     Infected(usize), // time left
     NotInfected(f32) // immunity
 }
 
 #[derive(Clone)]
+#[derive(Debug)]
 struct Node {
     status: AgentStatus,
     infections: Vec<InfectionStatus>
@@ -80,8 +89,24 @@ impl Graph {
             }).count()
     }
 
-    fn simulate_basic_looped_stochastic(&mut self, steps: usize, diseases: &[Disease]) {
-        for _ in 0..steps {
+    // weights elements = probability of meeting at a given timestep
+    // output elements = number of timesteps until row meets column
+    // Sampling negative binomial distribution in same manner as random_negative_binomial() in
+    // https://github.com/numpy/numpy/blob/master/numpy/random/src/distributions/distributions.c
+    fn deterministic_weights(&self, total_timesteps: usize) -> Array2<isize> {
+        let mut rng = thread_rng();
+        let mut determ_weights = Array2::zeros((self.weights.shape()[0], 2));
+        for (idx, p) in self.weights.indexed_iter() {
+            determ_weights[idx] = if *p > 0.0 {
+                    let y = rng.sample(Gamma::new(1.0, ((1.0 - *p) / *p) as f64));
+                    rng.sample(Poisson::new(y)) as isize}
+                else {-1}
+        }
+        determ_weights
+    }
+
+    fn simulate_basic_looped_stochastic(&mut self, steps: usize, diseases: &[&Disease]) {
+        for t in 0..steps {
             let mut new_nodes: Vec<Node> = Vec::new();
             // For each node and its outgoing edges
             for (nodenum, (node, adj_weights)) in self.nodes.iter().zip(self.weights.outer_iter()).enumerate() {
@@ -137,27 +162,27 @@ impl Graph {
                 new_nodes.push(new_node);
             }
             self.nodes = new_nodes;
-            println!("{} dead", self.dead_count());
-            println!("{} infected", self.infected_count(0));
+            println!("T{}: {} dead, {} infected", t, self.dead_count(), self.infected_count(0));
         }
     }
 
-    fn simulate_basic_mat_stochastic(&mut self, steps: usize, diseases: &[Disease]) {
-        // TODO: immunity
-        for _ in 0..steps {
+    fn simulate_basic_mat_stochastic(&mut self, steps: usize, diseases: &[&Disease]) {
+        for t in 0..steps {
             // TODO: matrix*diag can be more efficient (and no alloc of second matrix)
-            let mut node_transmitivity = Array2::<f32>::zeros((self.nodes.len(), self.nodes.len()));
             let node_transmitivity_iter = self.nodes.iter().map(|n| match n.infections[0] {
                 InfectionStatus::Infected(t) => (*diseases[0].shedding_fun)(t as isize) * diseases[0].transmission_rate,
                 _ => 0.0
             });
+            let mut node_transmitivity = Array2::<f32>::zeros((self.nodes.len(), self.nodes.len()));
             for (i, t) in node_transmitivity_iter.enumerate() {
                 node_transmitivity[(i, i)] = t;
             }
             let mut pr_transmissions = self.weights.dot(&node_transmitivity);
             pr_transmissions.mapv_inplace(|p| (1.0 - p).log2());
-            let log_pr_infections = pr_transmissions.sum_axis(Axis(0)).to_vec();
-            
+            let log_pr_infections = pr_transmissions.sum_axis(Axis(1)).to_vec();
+
+            //println!("t: {}, n_infection_prs: {:?}", t, log_pr_infections.iter().map(|l| (2.0 as f32).powf(*l)).collect::<Vec<f32>>());
+
             self.nodes = self.nodes.iter().zip(log_pr_infections).map(|(n, ilpr)| match n.status {
                 AgentStatus::Asymptomatic => {
                     let mut died = false;
@@ -170,12 +195,17 @@ impl Graph {
                                 InfectionStatus::NotInfected(diseases[0].post_infection_immunity)
                             },
                         InfectionStatus::Infected(t) => InfectionStatus::Infected(t-1),
-                        InfectionStatus::NotInfected(immunity) => 
-                            if random::<f32>().log2() < ilpr {
+                        InfectionStatus::NotInfected(immunity) => {
+                            //println!("ilpr: {}, imm: {}", ilpr, immunity);
+                            // TODO: Modify weight matrix to reflect immunity
+                            if random::<f32>() < (1.0 - (2.0 as f32).powf(ilpr)) * (1.0 - immunity) {
+                                //println!("inf");
                                 InfectionStatus::Infected(diseases[0].infection_length)
                             } else {
+                                //println!("no inf");
                                 n.infections[0]
                             }
+                        }
                     };
                     Node {
                         status: if died {
@@ -188,13 +218,12 @@ impl Graph {
                 },
                 AgentStatus::Dead => n.clone()
             }).collect();
-            println!("{} dead", self.dead_count());
-            println!("{} infected", self.infected_count(0));
+            println!("T{}: {} dead, {} infected", t, self.dead_count(), self.infected_count(0));
         }
     }
 
-    fn simulate_basic_looped_deterministic(&mut self, steps: usize, diseases: &[Disease]) {
-        for _ in 0..steps {
+    fn simulate_basic_looped_deterministic(&mut self, steps: usize, diseases: &[&Disease]) {
+        for t in 0..steps {
             let mut new_nodes: Vec<Node> = Vec::new();
             // For each node and its outgoing edges
             for (nodenum, (node, adj_weights)) in self.nodes.iter().zip(self.weights.outer_iter()).enumerate() {
@@ -250,18 +279,32 @@ impl Graph {
                 new_nodes.push(new_node);
             }
             self.nodes = new_nodes;
-            println!("{} dead", self.dead_count());
-            println!("{} infected", self.infected_count(0));
+            println!("T{}: {} dead, {} infected", t, self.dead_count(), self.infected_count(0));
         }
     }
 }
 
-fn main() {
+fn new_sim_graph(n: usize, infection: &Disease)  -> Graph {
     let mut nodes: Vec<Node> = Vec::new();
-    nodes.resize(100, Node {
+    nodes.resize(n-1, Node {
         status: AgentStatus::Asymptomatic,
         infections: vec![InfectionStatus::NotInfected(0.1)]
     });
+    
+    nodes.push(Node {
+        status: AgentStatus::Asymptomatic,
+        infections: vec![InfectionStatus::Infected(infection.infection_length)]
+    });
+    Graph::new_uniform_from_nodes(nodes, 0.3)
+}
+
+fn main() -> io::Result<()> {
+    let a = array![[1,2,3],[4,5,6]];
+    println!("{:?}", a.sum_axis(Axis(1)).to_vec());
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
     let flu = Disease {
         name: "flu",
         transmission_rate: 0.3,
@@ -270,12 +313,18 @@ fn main() {
         post_infection_immunity: 0.99,
         shedding_fun: Box::new(|d| if d > 0 {1.0 / d as f32} else {0.0})
     };
-    nodes.push(Node {
-        status: AgentStatus::Asymptomatic,
-        infections: vec![InfectionStatus::Infected(flu.infection_length)]
-    });
-    let mut graph = Graph::new_uniform_from_nodes(nodes, 0.3);
-    graph.simulate_basic_looped_stochastic(200, &[flu]);
+
+    let mut graph = new_sim_graph(100, &flu);
+    
+    graph.simulate_basic_looped_stochastic(200, &[&flu]);
     println!("{} dead", graph.dead_count());
     println!("{} infected", graph.infected_count(0));
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    
+    let mut graph = new_sim_graph(100, &flu);
+    graph.simulate_basic_mat_stochastic(200, &[&flu]);
+    println!("{} dead", graph.dead_count());
+    println!("{} infected", graph.infected_count(0));
+    Ok(())
 }
